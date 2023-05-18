e5fed35b562267da1dc106b3de817dbc15510317
Mauro de Oliveira Andre <mauroolivere@hotmail.com>
2023-05-18 13:20:54 +0100

    Initial Commit

---
+++ Pyt/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pyt
+//
+//  Created by Mauro de Oliveira  Andre on 18/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pyt/PytApp.swift
@@ -0,0 +1,17 @@
+//
+//  PytApp.swift
+//  Pyt
+//
+//  Created by Mauro de Oliveira  Andre on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PytApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
